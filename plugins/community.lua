8d554745ec87db1aeb162fa85252e7a810ea22f6
eginoy <eginoy.dev@gmail.com>
2023-12-08 22:12:07 +0900

    add github-copilot plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -18,4 +18,22 @@ return {
   { import = "astrocommunity.pack.svelte" },
   { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.yaml" },
+
+  --copilog
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
 }
